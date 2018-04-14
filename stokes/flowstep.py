3a8f787b27dcfe7d4b49c7f2bb4f71dab833dd1e
Ed Bueler <elbueler@alaska.edu>
2018-04-13 22:09:51 -0800

    playing with stuff ... need to have the n=1 case reduce, for a slab, reduce to the linflowstep.py method

--- stokes/flowstep.py
+++ stokes/flowstep.py
@@ -4,7 +4,7 @@
 # Solve glacier bedrock-step Glen-Stokes problem.
 # See mccarthy/projects/2018/flowstep/README.md.
 
-# Usage with default bedstep:
+# Default usage:
 #     $ ./genstepmesh.py glacier.geo      # create domain
 #     $ gmsh -2 glacier.geo               # mesh domain
 #     $ source ~/firedrake/bin/activate
@@ -31,13 +31,16 @@ parser.add_argument('-elements', metavar='X', default=defaultmix, choices=mixcho
                     help='stable mixed finite elements from: %s (default=%s)' % (','.join(mixchoices),defaultmix) )
 parser.add_argument('-f', metavar='ROOT', default=defaultf,
                     help='input/output file name root (default=%s)' % defaultf)
-parser.add_argument('-bs', type=float, default=120.0, metavar='BS',
+parser.add_argument('-bs', type=float, default=120.0, metavar='X',
                     help='height of bed step (m; default = 120.0)')
 parser.add_argument('-initonly', action='store_true')
+parser.add_argument('-n_glen', type=float, default=3.0, metavar='X',
+                    help='Glen flow law exponent (default = 3.0)')
 args, unknown = parser.parse_known_args()
 inname = args.f + '.msh'
 outname = args.f + '.pvd'
 bs = args.bs
+n_glen = args.n_glen
 
 from firedrake import *
 
@@ -45,14 +48,14 @@ from firedrake import *
 secpera = 31556926.0       # seconds per year
 g = 9.81                   # m s-2
 rho = 910.0                # kg m-3
-A_ice = 3.1689e-24         # Pa-3 s-1; EISMINT I value of ice softness
-B_ice = A_ice**(-1.0/3.0)  # Pa s0.33333;  ice hardness
+A_ice = 3.1689e-24         # Pa-3 s-1; EISMINT I value of ice softness; tied to n=3
+B_ice = A_ice**(-1.0/3.0)  # Pa s(1/3);  ice hardness
 
 # input mesh and define geometry
 print('reading mesh from %s ...' % inname)
 mesh = Mesh(inname)
 print('mesh has %d vertices and %d elements' % (mesh.num_vertices(),mesh.num_cells()))
-top_id = 32
+base_id = 32
 inflow_id = 33
 outflow_id = 34
 H = 400.0
@@ -77,7 +80,10 @@ v,q = TestFunctions(Z)
 # define body force
 f_body = Constant((g * rho * sin(alpha), - g * rho * cos(alpha)))
 
-# right side outflow: apply hydrostatic normal force, nonhomogeneous Neumann
+# Dirichlet boundary condition applied on base
+noslip = Constant((0.0, 0.0))
+
+# right side outflow: apply hydrostatic normal force; nonhomogeneous Neumann
 x,z = SpatialCoordinate(mesh)
 outflow_sigma = as_vector([- rho * g * cos(alpha) * (H - z), 0.0])
 
@@ -85,15 +91,15 @@ outflow_sigma = as_vector([- rho * g * cos(alpha) * (H - z), 0.0])
 def initialiterate(Z):
     # assume Newtonian slab-on-slope inflow
     D_typical = 10.0 / secpera
-    nu_e = B_ice * D_typical**(-2.0/3.0)  # effective viscosity
-    #print('effective viscosity %.2e Pa s' % nu_e)
-    Clin = rho * g * sin(alpha) / nu_e
-    inflow_lin = as_vector([Clin * ((H + bs) * (z - bs) + (bs*bs - z*z)/2.0), 0.0])
+    nulin = B_ice * D_typical**(-2.0/3.0)  # effective viscosity
+    print('effective viscosity in initialization %.2e Pa s' % nulin)
+    Clin = rho * g * sin(alpha) / nulin
+    inflowlin = as_vector([Clin * ((H + bs) * (z - bs) + (bs*bs - z*z)/2.0), 0.0])
     ulin,plin = TrialFunctions(Z)
-    bcslin = [ DirichletBC(Z.sub(0), noslip, (top_id,)),
-               DirichletBC(Z.sub(0), inflow_lin, (inflow_id,)) ]
+    bcslin = [ DirichletBC(Z.sub(0), noslip, (base_id,)),
+               DirichletBC(Z.sub(0), inflowlin, (inflow_id,)) ]
     # Newtonian weak form
-    alin = ( nu_e * inner(grad(ulin), grad(v)) - plin * div(v) - div(ulin) * q ) * dx
+    alin = ( nulin * inner(grad(ulin), grad(v)) - plin * div(v) - div(ulin) * q ) * dx
     Llin = inner(f_body, v) * dx + inner(outflow_sigma, v) * ds(outflow_id)
     print('solving linear variational problem for initial iterate ...')
     uplin = Function(Z)   # put solution here
@@ -115,27 +121,21 @@ def initialiterate(Z):
                              "fieldsplit_1_pc_type": "none"})
     return uplin
 
-# define the nonlinear weak form F(u;v):
-up = Function(Z)       # put solution here; *not* TrialFunctions(Z)
+# put solution here
+up = Function(Z)       # *not* TrialFunctions(Z)
 u,p = split(up)        # up.split() not equivalent here?
-Du = 0.5 * (grad(u) + grad(u).T)
-absDu2 = 0.5 * inner(Du, Du)
 
+# define the nonlinear weak form F(u;v)
 D_typical = 10.0 / secpera
 eps2 = 0.0001 * D_typical**2.0
-nu = B_ice * (absDu2 + eps2)**(-1.0/3.0)
+Du = 0.5 * (grad(u) + grad(u).T)
+nu = B_ice * (0.5 * inner(Du, Du) + eps2)**(-1.0/n_glen)
 #nu = B_ice * D_typical**(-2.0/3.0)  # effective viscosity
-
 Dv = 0.5 * (grad(v) + grad(v).T)
 F = ( inner(2.0 * nu * Du, Dv) - p * div(v) - div(u) * q - inner(f_body, v) ) * dx \
     - inner(outflow_sigma, v) * ds(outflow_id)
-#F = ( inner(2.0 * nu * grad(u), grad(v)) - p * div(v) - div(u) * q - inner(f_body, v) ) * dx \
-#    - inner(outflow_sigma, v) * ds(outflow_id)
 
-# Dirichlet boundary conditions, defined on the velocity space, apply
-# on base and inflow
-noslip = Constant((0.0, 0.0))
-
+# inflow boundary condition
 Hin = H + bs
 if False:
     # assume Newtonian slab-on-slope inflow
@@ -144,10 +144,10 @@ if False:
     inflow_u = as_vector([Clin * (Hin * (z - bs) + (bs*bs - z*z)/2.0), 0.0])
 else:
     # assume slab-on-slope inflow
-    C = 0.5 * A_ice * (rho * g * sin(alpha))**3.0
-    inflow_u = as_vector([C * (Hin**4.0 - (Hin - z)**4.0), 0.0])
+    C = 2.0 * A_ice * (rho * g * sin(alpha))**n_glen / (n_glen + 1.0)
+    inflow_u = as_vector([C * (H**(n_glen+1.0) - (Hin - z)**(n_glen+1.0)), 0.0])
 
-bcs = [ DirichletBC(Z.sub(0), noslip, (top_id,)),
+bcs = [ DirichletBC(Z.sub(0), noslip, (base_id,)),
         DirichletBC(Z.sub(0), inflow_u, (inflow_id,)) ]
 
 # solve
