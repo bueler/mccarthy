3f3c4f524ef63233d1e4e470785829892e198c83
Ed Bueler <elbueler@alaska.edu>
2018-04-25 20:33:25 -0800

    redo numbering ... probably last version was o.k.

--- stokes/genstepmesh.py
+++ stokes/genstepmesh.py
@@ -6,16 +6,14 @@
 #   $ gmsh -2 glacier.geo
 #   ...
 # which generates glacier.msh.  (Use "gmsh glacier.geo" for GUI version.)
-# Then load mesh in python/firedrake:
+# One may inspect the mesh by loading it in python/firedrake:
 #   $ source firedrake/bin/activate
 #   (firedrake) $ ipython
 #   ...
 #   In [1]: from firedrake import *
 #   In [2]: Mesh('glacier.msh')
 # Main purpose is to solve the Stokes equations on this domain.  See
-# linflowstep.py:
-#   $ source firedrake/bin/activate
-#   (firedrake) $ ./linflowstep.py
+# flowstep.py.
 
 import numpy
 import argparse
@@ -45,64 +43,64 @@ print('writing bedrock-step geometry to file %s ...' % args.filename)
 geo.write('// geometry-description file created %s by %s using command\n//   %s\n\n'
           % (now,platform.node(),commandline) )
 
-# glacier domain parameters; distance in meters
+# domain distances in meters
 H = 400.0        # input and output thickness (z)
 L = 2000.0       # total along-flow length (x)
 Ls = 1300.0      # location of bedrock step (x)
 Ks = 150.0       # distance over which transition in surface happens (x)
 
-# mesh parameters
-lc = 400.0 / args.refine
+# set "characteristic lengths" which are used by gmsh to generate triangles
+lc = 300.0 / args.refine
 print('setting target mesh size of %g m' % lc)
-geo.write('lc = %f;\n' % lc)    # put target characteristic mesh size
-
-# points on boundary
-geo.write('Point(1) = {%f,%f,0,lc};\n' % (Ls,0.0))
-geo.write('Point(2) = {%f,%f,0,lc};\n' % (L,0.0))
-geo.write('Point(3) = {%f,%f,0,lc};\n' % (L,H))
-geo.write('Point(4) = {%f,%f,0,lc};\n' % (Ls+Ks,H))
-# FIXME  between Point(4) and Point(5) should be a nice smooth curve ... but what?
-geo.write('Point(5) = {%f,%f,0,lc};\n' % (Ls-4*Ks,H+bs))
-geo.write('Point(6) = {%f,%f,0,lc};\n' % (0.0,H+bs))
-geo.write('Point(7) = {%f,%f,0,lc};\n' % (0.0,bs))
+geo.write('lc = %f;\n' % lc)
 if abs(bs) >= 1.0:
     lc_corner = lc / args.refine_corner
     print('setting target mesh size of %g m at interior corner' % lc_corner)
     geo.write('lc_corner = %f;\n' % lc_corner)
-    geo.write('Point(8) = {%f,%f,0,lc_corner};\n' % (Ls,bs))
 else:
-    print('WARNING: bed step height |bs| < 1.0 so bed step removed')
+    print('WARNING: bed step height |bs| < 1.0 so bed step is removed')
+
+# points on boundary
+geo.write('Point(1) = {%f,%f,0,lc};\n' % (L,0.0))
+geo.write('Point(2) = {%f,%f,0,lc};\n' % (L,H))
+geo.write('Point(3) = {%f,%f,0,lc};\n' % (Ls+Ks,H))
+# FIXME  between Point(3) and Point(4) could be a nice smooth curve
+geo.write('Point(4) = {%f,%f,0,lc};\n' % (Ls-4*Ks,H+bs))
+geo.write('Point(5) = {%f,%f,0,lc};\n' % (0.0,H+bs))
+geo.write('Point(6) = {%f,%f,0,lc};\n' % (0.0,bs))
+if abs(bs) >= 1.0:
+    geo.write('Point(7) = {%f,%f,0,lc_corner};\n' % (Ls,bs))
+    geo.write('Point(8) = {%f,%f,0,lc_corner};\n' % (Ls,0.0))
 
 # lines along boundary
-loop = '9'
-for j in range(6):
-    geo.write('Line(%d) = {%d,%d};\n' % (j+9,j+1,j+2))
-    if j > 0:
-        loop += ',%d' % (j+9)
+geo.write('Line(11) = {1,2};\n')
+geo.write('Line(12) = {2,3};\n')
+geo.write('Line(13) = {3,4};\n')
+geo.write('Line(14) = {4,5};\n')
+geo.write('Line(15) = {5,6};\n')
 if abs(bs) >= 1.0:
-    geo.write('Line(15) = {7,8};\n')
-    geo.write('Line(16) = {8,1};\n')
-    loop += ',15,16'
+    geo.write('Line(16) = {6,7};\n')
+    geo.write('Line(17) = {7,8};\n')
+    geo.write('Line(18) = {8,1};\n')
 else:
-    geo.write('Line(15) = {7,1};\n')
-    loop += ',15'
+    geo.write('Line(16) = {6,1};\n')
 
 # loop and surface allows defining a 2D mesh
-geo.write('Line Loop(17) = {%s};\n' % loop)
-geo.write('Plane Surface(21) = {17};\n')
+if abs(bs) >= 1.0:
+    geo.write('Line Loop(21) = {11,12,13,14,15,16,17,18};\n')
+else:
+    geo.write('Line Loop(21) = {11,12,13,14,15,16};\n')
+geo.write('Plane Surface(31) = {21};\n')
 
 # "Physical" for marking boundary conditions
-geo.write('Physical Line(30) = {9};\n')         # base of ice downstream of step
-geo.write('Physical Line(31) = {10};\n')        # outflow (right side)
-geo.write('Physical Line(32) = {11,12,13};\n')  # stress-free boundary (top)
-geo.write('Physical Line(33) = {14};\n')        # inflow (left side)
-if abs(bs) >= 1.0:                              # base of ice upstream and step itself
-    geo.write('Physical Line(34) = {15,16};\n')
+geo.write('Physical Line(41) = {11};\n')        # outflow (right side)
+geo.write('Physical Line(42) = {12,13,14};\n')  # top (stress-free)
+geo.write('Physical Line(43) = {15};\n')        # inflow (left side)
+if abs(bs) >= 1.0:                              # base (no slip)
+    geo.write('Physical Line(44) = {16,17,18};\n')
 else:
-    geo.write('Physical Line(34) = {15};\n')
-
-# not clear following is needed
-geo.write('Physical Surface(41) = {21};\n')   # ensure all interior elements written(?)
+    geo.write('Physical Line(44) = {16};\n')
+geo.write('Physical Surface(51) = {31};\n')   # ensure all interior elements written(?)
 
 # finish up
 geo.close()
