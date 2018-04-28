095609ef592685f75caf6a3b5bf12b5c9fc46043
Ed Bueler <elbueler@alaska.edu>
2018-04-28 12:59:05 -0800

    great evidence for convergence in slab case for u (down to a few mm/a at h=12.5 m grid)

---
+++ stokes/study/convergeslab.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+# measure convergence on sequence of refining slab geometries
+# note viscosity regularization relevant
+# after setting-up firedrake in parent directory, run as:
+#    ./convergeslab.sh &> convergeslab.txt
+
+function runcase() {
+  CMD="../flowstep.py -eps $2 $1 -s_snes_max_it 200"
+  echo $CMD
+  rm -f tmp.txt
+  #/usr/bin/time -f "real %e" $CMD &> tmp.txt
+  $CMD &> tmp.txt
+  grep 'converged due to' tmp.txt
+  grep 'maximum velocity' tmp.txt
+  grep 'numerical error' tmp.txt
+  #grep real tmp.txt
+  rm -f tmp.txt
+}
+
+EPS=0.0001
+for REFINE in 0 1 2 4 8; do
+    MESH=slab$REFINE
+    if [ "$REFINE" -eq "0" ]; then
+        ../genstepmesh.py -hmesh 200.0 -bs 0.0 -o $MESH.geo | grep setting
+    else
+        ../genstepmesh.py -refine $REFINE -bs 0.0 -o $MESH.geo | grep setting
+    fi
+    gmsh -2 $MESH.geo | grep vertices
+    runcase $MESH.msh $EPS 
+done
+
