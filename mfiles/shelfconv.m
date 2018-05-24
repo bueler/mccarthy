6d9b28ae512382f9b42e680f309143072f0acfcf
Ed Bueler <elbueler@alaska.edu>
2018-05-23 18:26:39 -0800

    changes to comments and figure appearance

--- mfiles/shelfconv.m
+++ mfiles/shelfconv.m
@@ -17,12 +17,11 @@ loglog(dxkm,maxerr,'o-','markersize',16)
 pf = polyfit(log(dxkm),log(maxerr),1);
 hold on, loglog(dxkm,exp(pf(1)*log(dxkm)+pf(2)),'r:'), hold off
 grid on, xlabel('dx  (km)','fontsize',16)
-ylabel('maximum error  (m a^{-1})','fontsize',16)
+ylabel('maximum error  (m a-1)','fontsize',16)
 result = sprintf('convergence rate O(dx^{%.5f})\n',pf(1));
-disp(result),   text(2*dxkm(end-1),maxerr(end-1),result,'fontsize',18)
-
-return
-
-figure(2)  % on super-low resolution grid, show solution
-testshelf(20);  fprintf('\n')
+disp(result)
+text(2.0*dxkm(end-1),2.0*maxerr(end-1),result,'fontsize',18,'interpreter','latex')
+grid minor off
+set(gca,'xtick',[100 200 400 1000 2000 4000 8000]/1000)
+set(gca,'xticklabel',{'0.1','0.2','0.4','1','2','4','8'})
 
