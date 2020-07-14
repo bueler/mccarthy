ce6e9bc4994c41ca3a9bdd202ed805bff9505289
Ed Bueler <elbueler@alaska.edu>
2020-07-13 22:49:39 -0800

    convergence script uses -refine: simpler and generally better

--- stokes/study/convergeslab.sh
+++ stokes/study/convergeslab.sh
@@ -13,11 +13,12 @@ P=$1
 # iterations, but flow.py does not set up a grid hierarchy
 
 function runcase() {
-  CMD="mpiexec -n $P ../flow.py -mesh $1 -eps $2 -s_snes_converged_reason -s_snes_max_it 200"
+  CMD="mpiexec -n $P ../flow.py -mesh $1 -eps $2 -refine $3 -s_snes_converged_reason -s_snes_max_it 200"
   echo $CMD
   rm -f tmp.txt
   #/usr/bin/time -f "real %e" $CMD &> tmp.txt
   $CMD &> tmp.txt
+  grep 'elements' tmp.txt
   grep 'converged due to' tmp.txt
   grep 'flow speed' tmp.txt
   grep 'numerical errors' tmp.txt
@@ -26,14 +27,9 @@ function runcase() {
 }
 
 EPS=0.0001
-for REFINE in 0 1 2 4; do
-    MESH=slab$REFINE
-    if [ "$REFINE" -eq "0" ]; then
-        ../gendomain.py -hmesh 160.0 -bs 0.0 -o $MESH.geo | grep setting
-    else
-        ../gendomain.py -refine $REFINE -bs 0.0 -o $MESH.geo | grep setting
-    fi
-    gmsh -2 $MESH.geo | grep nodes
-    runcase $MESH.msh $EPS 
+../gendomain.py -hmesh 160.0 -bs 0.0 -coarsen_upperleft 1.0 -o base.geo
+gmsh -2 base.geo | grep Running
+for REFINE in 0 1 2 3; do
+    runcase base.msh $EPS $REFINE
 done
 
