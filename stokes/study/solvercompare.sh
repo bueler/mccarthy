4655f0c750a2d35da540498efc0bced171e12b83
Ed Bueler <elbueler@alaska.edu>
2020-07-17 16:45:10 -0800

    update solver comparison script

--- stokes/study/solvercompare.sh
+++ stokes/study/solvercompare.sh
@@ -4,24 +4,25 @@ set -e
 # compare solver performance on sequence of refining meshes
 
 # notes:
-#   1. Direct does not scale for higher res?
+#   1. Direct does not converge for higher res
 #   2. Testing of SchurGMGSelfp up to sequence 5 suggests optimality;
 #      only slower than SchurDirect up to -sequence 3.
 
-SEQ=2
+SEQ=3
+SOLVERS="SchurDirect SchurGMGSelfp SchurGMGMass SchurGMGMassFull"
 
 ../gendomain.py -o glacier.geo | grep writing
 gmsh -2 glacier.geo | grep Writing
 echo
 
-for PAC in SchurDirect SchurGMGSelfp Direct; do
-    CMD="../flow.py -mesh glacier.msh -sequence $SEQ -package $PAC -s_snes_converged_reason"
+for PAC in $SOLVERS; do
+    CMD="../flow.py -mesh glacier.msh -sequence $SEQ -package $PAC -s_snes_converged_reason -s_ksp_converged_reason"
     echo $CMD
 
     rm -f tmp.txt
     /usr/bin/time -f "real %e" $CMD &> tmp.txt
     grep "grid-sequencing" tmp.txt
-    grep "solve converged" tmp.txt
+    grep -B 1 "Nonlinear s_ solve converged" tmp.txt
     grep "real" tmp.txt
 done
 
