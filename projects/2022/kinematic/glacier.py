79d3a95e8fb9f91fd9b353631699556db475b404
Ed Bueler <elbueler@alaska.edu>
2022-05-20 14:19:50 -0800

    plots surface slope and velocity

--- projects/2022/kinematic/glacier.py
+++ projects/2022/kinematic/glacier.py
@@ -6,6 +6,9 @@ import numpy as np
 # t in seconds, x in meters, s(t,x) = H(t,x) in meters, u(t,x) in m/s
 secpera = 31556926.0
 n = 3.0
+rho = 910.0
+g = 9.81
+A = 1.0e-16/secpera    # EISMINT I value
 _T = 1000.0 * secpera  # 1000 a
 _H0 = 1000.0
 _L = 400.0 * 1.0e3     # 400 km
@@ -21,29 +24,49 @@ def L(t):
     return _L * (1.0 - 0.75 * np.sin(np.pi * t / _T))
 
 def _psi(t,x):   # vectorized over x
-    y = np.minimum(1.0, np.abs(x) / L(t))
-    return (n+1.0) * y - 1.0 + n * (1.0 - y)**_q - n * y**_q
+    psi = np.zeros(np.shape(x))
+    y = np.abs(x[np.abs(x)<L(t)]) / L(t)
+    psi[np.abs(x)<L(t)] = (n+1.0) * y - 1.0 + n * (1.0 - y)**_q - n * y**_q
+    return psi
 
 def s(t,x):   # vectorized over x
     _C = H0(t) / (n - 1.0)**_r
     return _C * _psi(t,x)**_r
 
-def _sx(t,x):  # vectorized over x
+def sx(t,x):  # vectorized over x
     _C = _r * H0(t) / (n - 1.0)**_r
-    y = np.abs(x) / L(t)
-    #FIXME return _C * _psi(t,x) *
-    return 0.0
+    gamma = np.zeros(np.shape(x))
+    gamma[np.abs(x)<L(t)] = _psi(t,x[np.abs(x)<L(t)])**(_r-1.0)
+    omega = np.zeros(np.shape(x))
+    y = np.abs(x[np.abs(x)<L(t)]) / L(t)
+    z = np.sign(x[np.abs(x)<L(t)]) / L(t)
+    omega[np.abs(x)<L(t)] = (n+1.0) * z - (n+1.0) * (1.0 - y)**(1.0/n) * z \
+                            - (n+1.0) * y**(1.0/n)  * z
+    return _C * gamma * omega
+
+def u(t,x):  # vectorized over x
+    _gam = 2.0 * A * (rho * g)**n / (n+1)
+    return - _gam * s(t,x)**(n+1.0) * sx(t,x)**n   # assumes n odd integer
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
     print('plotting glacier at t=0, %.0f, %.0f years ...' \
           % (0.25*_T/secpera,0.5*_T/secpera))
-    xx = np.linspace(-1.1*_L,1.1*_L,401)
+    xx = np.linspace(-1.1*_L,1.1*_L,801)
+    plt.subplot(3,1,1)
     for tt in [0.0, 0.25*_T, 0.5*_T]:
-        ss = s(tt,xx)
-        plt.plot(xx/1.0e3,ss,label='%.0f years' % (tt/secpera))
-    plt.xlabel('x  (km)')
-    plt.ylabel('s  (m)')
+        plt.plot(xx/1.0e3,s(tt,xx),
+                 label='%.0f years' % (tt/secpera))
+    plt.ylabel('elevation s  (m)')
     plt.legend()
+    plt.subplot(3,1,2)
+    for tt in [0.0, 0.25*_T, 0.5*_T]:
+        plt.plot(xx/1.0e3,sx(tt,xx),'.',ms=2.0)
+    plt.ylabel('surface slope s_x')
+    plt.subplot(3,1,3)
+    for tt in [0.0, 0.25*_T, 0.5*_T]:
+        plt.plot(xx/1.0e3,u(tt,xx)*secpera)
+    plt.ylabel('surface velocity u  (m/a)')
+    plt.xlabel('x  (km)')
     plt.show()
-    # TODO: compute s_x, u, ds/dt, a
+    # TODO: compute ds/dt, a
