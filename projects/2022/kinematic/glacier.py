f0699a2161bf5518223f63ae24a5542c862cdf39
Ed Bueler <elbueler@alaska.edu>
2022-05-23 16:56:16 -0800

    cleaned-up (and complete for now)

--- projects/2022/kinematic/glacier.py
+++ projects/2022/kinematic/glacier.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python3
 # (C) 2022 Ed Bueler
 
-# Generates synthetic, time-dependent glacier geometry, surface motion, and
-# climatic mass balance.  For documentation of formulas see synglac.pdf.  For
-# instructions for use see README.md.
+# Generates synthetic, time-dependent glacier geometry, horizontal surface
+# motion, and lumped climatic mass balance.  For documentation of formulas
+# see synglac.pdf.  For usage instructions see README.md.
 
 import numpy as np
 
 # t in seconds, x in meters, s(t,x) = H(t,x) in meters, u(t,x) in m/s
 secpera = 31556926.0
-n = 3.0
-rho = 910.0
-g = 9.81
+n = 3.0                # Glen exponent
+rho = 910.0            # ice density; kg m-3
+g = 9.81               # Earth gravity; m s-2
 A = 1.0e-16/secpera    # EISMINT I value
 T = 2000.0 * secpera   # 2000 a
-H0_0 = 3000.0          # 3000 m
+Hc_0 = 3000.0          # 3000 m
 L_0 = 400.0 * 1.0e3    # 400 km
 
-# derived constants
+# derived powers
 q = 1.0 + 1.0 / n
 r = n / (2.0 * n + 2.0)
 
 # time-dependent center surface elevation (thickness), and its derivative
-def H0(t):
-    return H0_0 * (1.0 - 0.5 * np.sin(np.pi * t / T))
+def Hc(t):
+    return Hc_0 * (1.0 - 0.5 * np.sin(np.pi * t / T))
 
-def dH0dt(t):
-    return - 0.5 * H0_0 * np.cos(np.pi * t / T) * (np.pi / T)
+def dHcdt(t):
+    return - 0.5 * Hc_0 * np.cos(np.pi * t / T) * (np.pi / T)
 
 # time-dependent glacier length, from center to margin, and its derivative
 def L(t):
@@ -37,47 +37,57 @@ def dLdt(t):
 
 # following functions of (t,x) are vectorized over x but not t
 
-# helper function psi(t,x), phi(t,x) and derivatives
-def _psi(t,x):
+# helper functions
+def _geticyy(t,x):
     icy = (np.abs(x) < L(t))
-    y = np.abs(x[icy]) / L(t)
+    return icy, (np.abs(x[icy]) / L(t))
+
+def _psi(t,x):
+    icy, y = _geticyy(t,x)
     psi = np.zeros(np.shape(x))
     psi[icy] = (n+1.0) * y - 1.0 + n * (1.0 - y)**q - n * y**q
     return psi
 
 def _phi(t,x):
-    icy = (np.abs(x) < L(t))
-    y = np.abs(x[icy]) / L(t)
+    icy, y = _geticyy(t,x)
     phi = np.zeros(np.shape(x))
     phi[icy] = (1.0 - y)**(1.0/n) + y**(1.0/n) - 1.0
     return phi
 
 def _dpsidt(t,x):
-    icy = (np.abs(x) < L(t))
-    y = np.abs(x[icy]) / L(t)
+    icy, y = _geticyy(t,x)
     dpsi = np.zeros(np.shape(x))
-    dpsi[icy] = (n+1.0) * y * (dLdt(t) / L(t)) * _phi(t,x[icy])
+    dpsi[icy] = (n+1.0) * (dLdt(t) / L(t)) * y * _phi(t,x)
     return dpsi
 
 def _dpsidx(t,x):
-    icy = (np.abs(x) < L(t))
+    icy, y = _geticyy(t,x)
     z = np.sign(x[icy]) / L(t)
     dpsi = np.zeros(np.shape(x))
-    dpsi[icy] = - (n+1.0) * z * _phi(t,x[icy])
+    dpsi[icy] = - (n+1.0) * z * _phi(t,x)
     return dpsi
 
 # surface elevation s(t,x) from formula (5.50) in van der Veen (2013)
 def s(t,x):
-    C = H0(t) / (n - 1.0)**r
-    return C * _psi(t,x)**r
+    return Hc(t) * (n-1.0)**(-r) * _psi(t,x)**r
 
-# surface slope ds/dx(t,x) = partial s / partial t
+# the time rate of change of the surface; dsdt = partial s / partial t
+# WARNING: negative power of zero if formula is used where not icy
+def dsdt(t,x):
+    C = (n-1.0)**(-r)
+    icy, _ = _geticyy(t,x)
+    ds = np.zeros(np.shape(x))
+    ds[icy] = C * ( dHcdt(t) * _psi(t,x[icy])**r \
+                    + r * Hc(t) * _psi(t,x[icy])**(r-1.0) * _dpsidt(t,x[icy]) )
+    return ds
+
+# surface slope; dsdx = partial s / partial x
 # WARNING: negative power of zero if formula is used where not icy
 def dsdx(t,x):
-    icy = (np.abs(x) < L(t))
+    C = (n - 1.0)**(-r)
+    icy, _ = _geticyy(t,x)
     ds = np.zeros(np.shape(x))
-    ds[icy] = r * H0(t) / (n - 1.0)**r * _psi(t,x[icy])**(r-1.0) \
-                                       * _dpsidx(t,x[icy])
+    ds[icy] = r * Hc(t) * C * _psi(t,x[icy])**(r-1.0) * _dpsidx(t,x[icy])
     return ds
 
 # horizontal velocity at surface u(t,x) from SIA formula (see notes)
@@ -85,13 +95,20 @@ def us(t,x):
     gam = 2.0 * A * (rho * g)**n / (n+1.0)
     return - gam * s(t,x)**(n+1.0) * dsdx(t,x)**n   # assumes n odd integer
 
-# the surface mass balance a_balance(t,x) would *be* the SMB if s(t,x)
+# the vertical surface rate atilde(t,x) is the actual SMB a(t,x) lumped
+# with the vertical velocity at the surface w|_s(t,x)
+# FIXME extend by ablation I think
+def atilde(t,x):
+    return dsdt(t,x) + us(t,x) * dsdx(t,x)
+
+# abalance(t,x) below would *be* the SMB if s(t,x)
 # were held at time t; see formula (5.51) in van der Veen (2013)
-# [in fact s(t,x) changes in time; atrue(t,x) is the actual SMB]
+# [in fact s(t,x) changes in time; atilde(t,x) is the actual SMB,
+# but lumped with w|_s]
 def abalance(t,x):
     icy = (np.abs(x) < L(t))
     Gam = 2.0 * A * (rho * g)**n / (n+2.0)
-    C = H0(t)**(2.0*n+2.0) * Gam * (2.0 * L(t) * (1.0 - 1.0/n))**(-n)
+    C = Hc(t)**(2.0*n+2.0) * Gam * (2.0 * L(t) * (1.0 - 1.0/n))**(-n)
     y = np.abs(x[icy]) / L(t)
     phi = y**(1.0/n) + (1.0 - y)**(1.0/n) - 1.0
     ell = (1.0-n) / n
@@ -100,23 +117,7 @@ def abalance(t,x):
     abal[icy] = (C / L(t)) * phi**(n-1.0) * omega
     return abal
 
-# the time rate of change of the surface; \partial s / \partial t
-# WARNING: negative power of zero if formula is used where not icy
-def dsdt(t,x):
-    C = (n-1.0)**(-r)
-    icy = (np.abs(x) < L(t))
-    ds = np.zeros(np.shape(x))
-    ds[icy] = C * ( dH0dt(t) * _psi(t,x[icy])**r \
-                   + r * H0(t) * _psi(t,x[icy])**(r-1.0) * _dpsidt(t,x[icy]) )
-    return ds
-
-# the surface mass balance atrue(t,x) is the actual SMB
-# FIXME extend by ablation I think
-def atrue(t,x):
-    # FIXME
-    return np.zeros(np.shape(x))
-
-# if this file is executed then it produces plots to illustrate
+# if glacier.py is executed then it produces plots to illustrate
 # all important fields
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
@@ -132,7 +133,7 @@ if __name__ == "__main__":
         plt.plot(xx/1.0e3,s(tt[j],xx),
                  label='%.0f years' % (tt[j]/secpera))
         plt.legend(loc='upper right')
-        plt.gca().set_ylim([-200.0,H0_0+200.0])
+        plt.gca().set_ylim([-200.0,Hc_0+200.0])
         plt.gca().set_yticks([0.0,1500.0,3000.0])
         if j < 4:
             plt.tick_params(
@@ -146,40 +147,53 @@ if __name__ == "__main__":
             plt.ylabel('elevation s  (m)')
     #plt.savefig('surfacesnaps.png',bbox_inches='tight')
 
-    # figure 2: s, s_x, u
+    # figure 2: s, s_t, s_x in one figure
     plt.figure()
     plt.subplot(3,1,1)
     for tt in [0.0, 0.25*T, 0.5*T]:
         plt.plot(xx/1.0e3,s(tt,xx),
                  label='%.0f years' % (tt/secpera))
-    plt.legend()
+    plt.legend(loc='upper right')
     plt.ylabel('elevation s  (m)')
     plt.subplot(3,1,2)
-    for tt in [0.0, 0.25*T, 0.5*T]:
-        plt.plot(xx/1.0e3,dsdx(tt,xx),'.',ms=2.0)
-    plt.ylabel('surface slope s_x')
-    plt.subplot(3,1,3)
-    for tt in [0.0, 0.25*T, 0.5*T]:
-        plt.plot(xx/1.0e3,us(tt,xx)*secpera)
-    plt.ylabel('surface velocity u  (m/a)')
-    plt.xlabel('x  (km)')
-
-    # figure 3: s_t, abalance, atrue
-    plt.figure()
-    plt.subplot(3,1,1)
     for tt in [0.0, 0.25*T, 0.5*T]:
         plt.plot(xx/1.0e3,dsdt(tt,xx)*secpera,'.',ms=2.0,
                  label='%.0f years' % (tt/secpera))
     plt.ylabel('surface rate s_t  (m/a)')
-    plt.legend()
-    plt.subplot(3,1,2)
+    plt.gca().set_ylim([-20.0,2.0])    # compare range for atilde
+    plt.subplot(3,1,3)
+    for tt in [0.0, 0.25*T, 0.5*T]:
+        plt.plot(xx/1.0e3,dsdx(tt,xx),'.',ms=2.0)
+    plt.ylabel('surface slope s_x')
+    plt.xlabel('x  (km)')
+
+    # figure 3: us and us*s_x in one figure
+    plt.figure()
+    plt.subplot(2,1,1)
+    for tt in [0.0, 0.25*T, 0.5*T]:
+        plt.plot(xx/1.0e3,us(tt,xx)*secpera,
+                 label='%.0f years' % (tt/secpera))
+    plt.legend(loc='lower right')
+    plt.ylabel('surface velocity u_s  (m/a)')
+    plt.subplot(2,1,2)
+    for tt in [0.0, 0.25*T, 0.5*T]:
+        plt.plot(xx/1.0e3,us(tt,xx)*dsdx(tt,xx)*secpera,'.',ms=2.0)
+    plt.ylabel('product u_s * s_x  (m/a)')
+    plt.xlabel('x  (km)')
+
+    # figure 4: atilde, abalance
+    plt.figure()
+    plt.subplot(2,1,1)
+    for tt in [0.0, 0.25*T, 0.5*T]:
+        plt.plot(xx/1.0e3,atilde(tt,xx)*secpera,'.',ms=2.0,
+                 label='%.0f years' % (tt/secpera))
+    plt.legend(loc='lower right')
+    plt.ylabel('atilde  (m/a)')
+    plt.gca().set_ylim([-20.0,2.0])    # compare range for dsdt
+    plt.subplot(2,1,2)
     for tt in [0.0, 0.25*T, 0.5*T]:
         plt.plot(xx/1.0e3,abalance(tt,xx)*secpera)
     plt.ylabel('balance a  (m/a)')
-    plt.subplot(3,1,3)
-    for tt in [0.0, 0.25*T, 0.5*T]:
-        plt.plot(xx/1.0e3,atrue(tt,xx)*secpera)
-    plt.ylabel('true a  (m/a)')
     plt.xlabel('x  (km)')
 
     plt.show()
