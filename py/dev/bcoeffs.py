c7e1fdc70c8cb7e4aa8405ead1658857ac842b53
Ed Bueler <elbueler@alaska.edu>
2024-05-18 13:27:10 -0400

    make it do stuff ... next decide what I want it to do

--- py/dev/bcoeffs.py
+++ py/dev/bcoeffs.py
@@ -1,37 +1,37 @@
 # generate coefficients in b(x) polynomial:
-#   b(x) = a2 * (x-z)**2 + a3 * (x-z)**3 + a4 * (x-z)**4 + a5 * (x-z)**5
+#   b(x) = c2 * (x-z)**2 + c3 * (x-z)**3 + c4 * (x-z)**4 + c5 * (x-z)**5
 # where z = L/2, so that b(z)=b'(z)=0 and
 #     b0 = b(0)
 #    -m0 = b'(0)
 #   b0/3 = b(L)
 #      0 = b'(L)
-# in fact a5=0, so b(x) is quartic
+# in fact c5=0, so b(x) is quartic
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 L, b0, m0 = 20.0e3, 500.0, 0.1
 
+# solve linear system and print coefficients
 z = L/2
 Amat = np.array([[z**2,  -z**3,    z**4,  -z**5],
                  [-2*z, 3*z**2, -4*z**3, 5*z**4],
                  [z**2,   z**3,    z**4,   z**5],
                  [ 2*z, 3*z**2,  4*z**3, 5*z**4]])
 brhs = np.array([b0, -m0, b0/3, 0.0])
-a2, a3, a4, a5 = tuple(np.linalg.solve(Amat,brhs))
-assert abs(a5 * z**3 / a2) < 1.0e-14
-a5 = 0.0
+c2, c3, c4, c5 = tuple(np.linalg.solve(Amat, brhs))
+assert abs(c5 * z**3 / c2) < 1.0e-14
+c5 = 0.0
+print(f'c2, c3, c4 = {c2:.3e}, {c3:.3e}, {c4:.3e}')
 
-# result: this block computes b(x)
-print(f'a2={a2:.3e}, a3={a3:.3e}, a4={a4:.3e}')
+# result
 def b(x):
     x = np.array(x)
-    z = L / 2
-    return (x-z)**2 * (a2 + a3 * (x-z) + a4 * (x-z)**2)
+    z, c2, c3, c4 = L / 2, 4.167e-06, -1.667e-10, -8.333e-15
+    return (x-z)**2 * (c2 + c3 * (x-z) + c4 * (x-z)**2)
 
 # visualize to confirm
 x = np.linspace(-1e3, L+1e3, 1001)
 plt.plot(x, b(x))
 plt.plot([0,L], b([0,L]), 'b+')
 plt.show()
-
--- py/movesurface.py
+++ py/movesurface.py
@@ -1,14 +1,37 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 L = 20.0e3
 J = 20
 secpera = 31556925.0
-dt = 1.0
+dt = 3.0 * secpera
 
-x = np.linspace(0.0, L, J+1)
+
+def a(x):
+    return 1.0e-7 - 2.0e-7 * x / L
+
+
+def b(x):
+    z, c2, c3, c4 = L / 2, 4.167e-06, -1.667e-10, -8.333e-15
+    return (x - z) ** 2 * (c2 + c3 * (x - z) + c4 * (x - z) ** 2)
+
+
+def u(x):
+    return 2.0e-7 + 0.0 * x
+
+
+x = np.linspace(0.0, L, J + 1)
+dx = x[1] - x[0]
 xplot = np.linspace(0.0, L, 1001)
 
-b = 0.0 * x # FIXME design it
-s = 0.0 * x # FIXME data for initial surface
+# evolution loop
+s = b(0.0) - 200.0 * x / L + 50.0 * np.sin(10.0 * x / L)
+plt.plot(xplot, b(xplot))
+plt.plot(x, s, "-o")
+for k in range(4):
+    snew = s.copy()
+    snew[1:] += dt * (a(x[:-1]) - u(x[:-1]) * (s[1:] - s[:-1]) / dx)
+    s = snew
+    plt.plot(x, s, "-o")
 
-#snew = s + 
+plt.show()
